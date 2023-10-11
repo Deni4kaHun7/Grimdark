2817f8b472c9c28dfd5b5ac908de4a59d12556a0
Deni4kaHun7 <denis.serkes@gmail.com>
2023-10-11 19:20:45 +0200

    Combat files
    Combat.cs
    Input System
    Animation

---
+++ Assets/Scripts/Combat.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Combat : MonoBehaviour
+{
+    private PlayerControls playerControls;
+    private Animator animator;
+
+    private void Awake() {
+        playerControls = new PlayerControls();
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnEnable() {
+        playerControls.Enable();   
+    }
+
+    private void Start() {
+        playerControls.Combat.Attack.started += _ => Attack();
+    }
+
+    private void Attack(){
+        animator.SetTrigger("attack");
+        animator.SetTrigger("attack");
+    }
+}
