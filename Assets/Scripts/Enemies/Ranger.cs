0df1a30d08c2669ed73e03668719c34d82c058c3
Deni4kaHun7 <denis.serkes@gmail.com>
2023-11-12 19:52:16 +0100

    added animations

--- Assets/Scripts/Enemies/Ranger.cs
+++ Assets/Scripts/Enemies/Ranger.cs
@@ -6,8 +6,21 @@ public class Ranger : MonoBehaviour, IEnemy
 {
     [SerializeField] private GameObject bulletPrefab;
     [SerializeField] private GameObject bulletPosition;
+    private EnemyPathfinding enemyPathfinding;
+    private Animator animator;
+    private Vector2 moveDir;
+
+    private void Awake() {
+        animator = GetComponent<Animator>();
+        enemyPathfinding = GetComponent<EnemyPathfinding>();
+    }
+
+    private void Update() {
+        FlipColliderDirection();
+    }
 
     public void Attack(){
+        animator.SetTrigger("attack");
         Vector2 tartgetDirection = Player_Movement.Instance.transform.position - transform.position;
 
         GameObject newBullet = Instantiate(bulletPrefab, bulletPosition.transform.position, Quaternion.identity);
@@ -15,6 +28,11 @@ public class Ranger : MonoBehaviour, IEnemy
     }
 
     public void FlipColliderDirection(){
-        return;
+        moveDir = enemyPathfinding.moveDir;
+        if (moveDir.x > 0) {
+            bulletPosition.transform.position = new Vector3(0.5f, 0f, 0f);
+        } else if (moveDir.x < 0) {
+            bulletPosition.transform.position = new Vector3(-0.5f, 0f, 0f);
+        }
     }
 }
