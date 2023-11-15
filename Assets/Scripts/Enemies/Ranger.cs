4e3f66672174cec1b96da5d724265d8d055874bb
Deni4kaHun7 <denis.serkes@gmail.com>
2023-11-15 14:36:37 +0100

    fixed bug with bulletPosition

--- Assets/Scripts/Enemies/Ranger.cs
+++ Assets/Scripts/Enemies/Ranger.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class Ranger : MonoBehaviour, IEnemy
 {
     [SerializeField] private GameObject bulletPrefab;
-    [SerializeField] private GameObject bulletPosition;
+    [SerializeField] private GameObject bulletPositionLeft;
+    [SerializeField] private GameObject bulletPositionRight;
+    private GameObject newBullet;
     private EnemyPathfinding enemyPathfinding;
     private Animator animator;
     private Vector2 moveDir;
@@ -20,21 +22,26 @@ public class Ranger : MonoBehaviour, IEnemy
     }
 
     public void Attack(){
+        if(bulletPositionLeft.activeSelf == true){
+            newBullet = Instantiate(bulletPrefab, bulletPositionLeft.transform.position, Quaternion.identity);
+        }else {
+            newBullet = Instantiate(bulletPrefab, bulletPositionRight.transform.position, Quaternion.identity);
+        }
         animator.SetTrigger("attack");
         Vector2 tartgetDirection = Player_Movement.Instance.transform.position - transform.position;
 
-        GameObject newBullet = Instantiate(bulletPrefab, bulletPosition.transform.position, Quaternion.identity);
+        
         newBullet.transform.right = tartgetDirection;
     }
 
     public void FlipColliderDirection(){
         moveDir = enemyPathfinding.moveDir;
         if (moveDir.x > 0) {
-            //bulletPosition.transform.rotation = Quaternion.Euler(0, -180, 0);
-            //bulletPosition.transform.position = new Vector3(1, 0, 0);
+            bulletPositionLeft.SetActive(false);
+            bulletPositionRight.SetActive(true);
         } else if (moveDir.x < 0) {
-            //bulletPosition.transform.position = new Vector3(-0.5f, 0f, 0f);
-            //bulletPosition.transform.rotation = Quaternion.Euler(0, 0, 0);
+            bulletPositionLeft.SetActive(true);
+            bulletPositionRight.SetActive(false);
         }
     }
 }
